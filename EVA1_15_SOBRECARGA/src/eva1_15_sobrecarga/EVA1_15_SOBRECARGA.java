d9a5cd60a7f7c9ca28c2f0317163fe3e2c314ae6
JulioJacinto890 <124766374+JulioJacinto890@users.noreply.github.com>
2023-02-28 01:03:24 -0600

    Add files via upload

---
+++ EVA1_15_SOBRECARGA/src/eva1_15_sobrecarga/EVA1_15_SOBRECARGA.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_15_sobrecarga;
+
+/**
+ *
+ * @author hp
+ */
+public class EVA1_15_SOBRECARGA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*DIferentes println: son 10 tipos de  datos */
+        /*para crear sobrecargas  de metodos: */
+        System.out.println("Hola mundo");
+        System.out.println(100);
+        System.out.println(true);
+        System.out.println(args);
+        
+    }
+    
+}
